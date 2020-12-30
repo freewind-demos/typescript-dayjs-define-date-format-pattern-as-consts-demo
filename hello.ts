d479e49f63fbe9e25320560d33c731300eebf386
Freewind <nowindlee@gmail.com>
2020-12-30 21:05:44 +0800

    keyof typeof

--- hello.ts
+++ hello.ts
@@ -1,17 +1,17 @@
 import dayjs from 'dayjs';
 
 const PATTERNS = {
-  'M/D/YYYY': 'M/D/YYYY' as DateFormatPattern,
-  'MM/DD/YYYY': 'MM/DD/YYYY' as DateFormatPattern,
-  'YYYY-MM-DD': 'YYYY-MM-DD' as DateFormatPattern,
+  'M/D/YYYY': 'M/D/YYYY',
+  'MM/DD/YYYY': 'MM/DD/YYYY',
+  'YYYY-MM-DD': 'YYYY-MM-DD',
 }
 
-type DateFormatPattern = 'M/D/YYYY' | 'MM/DD/YYYY' | 'YYYY-MM-DD'
+type DateFormatPattern = keyof typeof PATTERNS;
 
 function formatDate(date: Date, pattern: DateFormatPattern): string {
   return dayjs(date).format(pattern)
 }
 
-const formattedDate = formatDate(new Date(), PATTERNS['YYYY-MM-DD'])
+const formattedDate = formatDate(new Date(), 'YYYY-MM-DD')
 console.log("### formattedDate: ", formattedDate);
 
